14db740ed568b8e14e181698968005125ce626f2
Ellie Ringwood <123974599+Ellie-Ringwood@users.noreply.github.com>
2024-08-17 16:22:28 +0100

    Start to multiplayer
    Netcode for gameobjects used
    created networkManager object
    created player prefab
    created manager script
    created player script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Move()
+    {
+        Debug.Log("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
