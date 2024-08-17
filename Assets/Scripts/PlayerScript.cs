4cb0af594982571c70c9cad802d2272a8e917167
Ellie Ringwood <123974599+Ellie-Ringwood@users.noreply.github.com>
2024-08-17 16:56:57 +0100

    Multiple players can be in one game

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,12 +12,22 @@ public class PlayerScript : MonoBehaviour
 
     public void Move()
     {
+        var x = 10;
+
+        transform.Translate(x, 0, 0);
         Debug.Log("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+    void OnMove()
+    {
+        Debug.Log("Input");
     }
 }
